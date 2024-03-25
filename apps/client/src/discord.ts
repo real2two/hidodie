2f36f6734d7ea922fe49884c05b7432c6f15ea48
real2two <26689496+real2two@users.noreply.github.com>
2024-03-25 16:02:55 -0400

    Split Discord SDK authentication operations into a separate file.

---
+++ apps/client/src/discord.ts
@@ -0,0 +1,112 @@
+import { DiscordSDK, RPCCloseCodes } from "@discord/embedded-app-sdk";
+import {
+  RouteBases,
+  RESTAPIPartialCurrentUserGuild,
+  RESTPatchAPIGuildMemberResult,
+} from "discord-api-types/v10";
+
+export const discordSdk = new DiscordSDK(
+  import.meta.env.VITE_DISCORD_CLIENT_ID,
+);
+
+export async function handleDiscordAuthentication() {
+  try {
+    const auth = await setupDiscordSdk();
+
+    return {
+      ...discordSdk,
+
+      user: auth.user,
+      member: await getVoiceMember(auth.access_token),
+      guild: await getVoiceGuild(auth.access_token),
+      channel: await getVoiceChannel(),
+    };
+  } catch (err) {
+    console.error(err);
+
+    discordSdk.close(
+      RPCCloseCodes.CLOSE_ABNORMAL,
+      "Authentication or authorization failure",
+    );
+
+    throw new Error("Authentication or authorization failure");
+  }
+}
+
+async function setupDiscordSdk() {
+  await discordSdk.ready();
+
+  // Authorize with Discord Client
+  const { code } = await discordSdk.commands.authorize({
+    client_id: import.meta.env.VITE_DISCORD_CLIENT_ID,
+    response_type: "code",
+    state: "",
+    prompt: "none",
+    scope: [
+      "identify",
+      "guilds",
+      "guilds.members.read",
+      "rpc.activities.write",
+    ],
+  });
+
+  // Retrieve an access_token from your activity's server
+  const response = await fetch("/api/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      code,
+    }),
+  });
+  const { access_token } = await response.json();
+
+  // Authenticate with Discord client (using the access_token)
+  const auth = await discordSdk.commands.authenticate({
+    access_token,
+  });
+
+  if (!auth) throw new Error("Authenticate command failed");
+  return auth;
+}
+
+export async function getUserGuilds(access_token: string) {
+  const response = await fetch(`${RouteBases.api}/users/@me/guilds`, {
+    headers: {
+      Authorization: `Bearer ${access_token}`,
+      "Content-Type": "application/json",
+    },
+  });
+  return (await response.json()) as RESTAPIPartialCurrentUserGuild[];
+}
+
+export async function getVoiceGuild(access_token: string) {
+  if (!discordSdk.guildId) return null;
+
+  const guilds = await getUserGuilds(access_token);
+  return guilds.find((g) => g.id === discordSdk.guildId);
+}
+
+export async function getVoiceChannel() {
+  if (!discordSdk.channelId || !discordSdk.guildId) return null;
+
+  return await discordSdk.commands.getChannel({
+    channel_id: discordSdk.channelId,
+  });
+}
+
+export async function getVoiceMember(access_token: string) {
+  if (!discordSdk.guildId) return null;
+
+  const response = await fetch(
+    `${RouteBases.api}/users/@me/guilds/${discordSdk.guildId}/member`,
+    {
+      headers: {
+        Authorization: `Bearer ${access_token}`,
+        "Content-Type": "application/json",
+      },
+    },
+  );
+  return (await response.json()) as RESTPatchAPIGuildMemberResult;
+}
--- apps/client/src/main.ts
+++ apps/client/src/main.ts
@@ -1,81 +1,28 @@
-import { DiscordSDK, RPCCloseCodes } from "@discord/embedded-app-sdk";
-
 import rocketLogo from "/rocket.png";
 import "./style.css";
 
-// Will eventually store the authenticated user's access_token
-let auth: Awaited<ReturnType<typeof discordSdk.commands.authenticate>>;
+import { handleDiscordAuthentication } from "./discord";
 
-const discordSdk = new DiscordSDK(import.meta.env.VITE_DISCORD_CLIENT_ID);
+document.querySelector("#app")!.innerHTML = `
+  <div>
+    <img src="${rocketLogo}" class="logo" alt="Discord" />
+    <h1>Hello, World!</h1>
+  </div>
+`;
 
-setupDiscordSdk()
-  .then(() => {
-    console.log("Discord SDK is authenticated");
-
-    appendVoiceChannelName();
-    appendGuildAvatar();
-  })
-  .catch((err) => {
-    console.error(err);
-    discordSdk.close(
-      RPCCloseCodes.CLOSE_ABNORMAL,
-      "Authentication or authorization failure",
-    );
-  });
-
-async function setupDiscordSdk() {
-  await discordSdk.ready();
-  console.log("Discord SDK is ready");
-
-  // Authorize with Discord Client
-  const { code } = await discordSdk.commands.authorize({
-    client_id: import.meta.env.VITE_DISCORD_CLIENT_ID,
-    response_type: "code",
-    state: "",
-    prompt: "none",
-    scope: ["identify", "guilds"],
-  });
-
-  // Retrieve an access_token from your activity's server
-  const response = await fetch("/api/token", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-      code,
-    }),
-  });
-  const { access_token } = await response.json();
-
-  // Authenticate with Discord client (using the access_token)
-  auth = await discordSdk.commands.authenticate({
-    access_token,
-  });
-
-  if (auth == null) {
-    throw new Error("Authenticate command failed");
-  }
-}
+const sdk = await handleDiscordAuthentication();
+const { user, member, guild, channel } = sdk;
+appendVoiceChannelName();
+appendGuildAvatar();
 
 async function appendVoiceChannelName() {
   const app = document.querySelector("#app")!;
 
   let activityChannelName = "Unknown";
-
-  // Requesting the channel in GDMs (when the guild ID is null) requires
-  // the dm_channels.read scope which requires Discord approval.
-  if (discordSdk.channelId != null && discordSdk.guildId != null) {
-    // Over RPC collect info about the channel
-    const channel = await discordSdk.commands.getChannel({
-      channel_id: discordSdk.channelId,
-    });
-    if (channel.name != null) {
-      activityChannelName = channel.name;
-    }
+  if (channel && channel.name) {
+    activityChannelName = channel.name;
   }
 
-  // Update the UI with the name of the current voice channel
   const textTagString = `Activity Channel: "${activityChannelName}"`;
   const textTag = document.createElement("p");
   textTag.innerHTML = textTagString;
@@ -85,25 +32,11 @@ async function appendVoiceChannelName() {
 async function appendGuildAvatar() {
   const app = document.querySelector("#app")!;
 
-  // 1. From the HTTP API fetch a list of all of the user's guilds
-  const guilds = (await fetch(`https://discord.com/api/v10/users/@me/guilds`, {
-    headers: {
-      // NOTE: we're using the access_token provided by the "authenticate" command
-      Authorization: `Bearer ${auth.access_token}`,
-      "Content-Type": "application/json",
-    },
-  }).then((response) => response.json())) as { id: string; icon: string }[];
-
-  // 2. Find the current guild's info, including it's "icon"
-  const currentGuild = guilds.find((g) => g.id === discordSdk.guildId);
-
-  // 3. Append to the UI an img tag with the related information
-  if (currentGuild != null) {
+  if (guild) {
     const guildImg = document.createElement("img");
     guildImg.setAttribute(
       "src",
-      // More info on image formatting here: https://discord.com/developers/docs/reference#image-formatting
-      `https://cdn.discordapp.com/icons/${currentGuild.id}/${currentGuild.icon}.webp?size=128`,
+      `https://cdn.discordapp.com/icons/${guild.id}/${guild.icon}.webp?size=128`,
     );
     guildImg.setAttribute("width", "128px");
     guildImg.setAttribute("height", "128px");
@@ -111,10 +44,3 @@ async function appendGuildAvatar() {
     app.appendChild(guildImg);
   }
 }
-
-document.querySelector("#app")!.innerHTML = `
-  <div>
-    <img src="${rocketLogo}" class="logo" alt="Discord" />
-    <h1>Hello, World!</h1>
-  </div>
-`;
