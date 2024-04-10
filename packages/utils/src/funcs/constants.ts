c016b22916031215254282b47cacf1dddf6337c9
real2two <26689496+real2two@users.noreply.github.com>
2024-04-10 17:45:16 -0400

    Removed the unnecessary guilds scope

--- packages/utils/src/funcs/constants.ts
+++ packages/utils/src/funcs/constants.ts
@@ -3,7 +3,7 @@ import { Types } from "@discord/embedded-app-sdk";
 /** These are the required scopes to authenticate to the activity */
 export const requiredScopes: Types.OAuthScopes[] = [
   "identify",
-  "guilds",
+  // "guilds",
   "guilds.members.read",
   "rpc.activities.write",
 ];
