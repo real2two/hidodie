bbed039c049a1864c37bf64e70ba17adb67625e8
real2two <26689496+real2two@users.noreply.github.com>
2024-05-15 21:21:28 -0400

    Separated canvas to a different file

---
+++ apps/client/src/js/main/canvas.ts
@@ -0,0 +1,46 @@
+import type { GameDocs } from "../../types";
+
+export function setupCanvas({ canvas, chatMessages }: GameDocs) {
+  // Gets the canvas
+  const ctx = canvas.getContext("2d", {
+    alpha: false,
+  }) as CanvasRenderingContext2D;
+
+  // Handles resizing
+  resize();
+  window.onresize = resize;
+
+  // Handles drawing
+  draw();
+
+  function draw() {
+    ctx.clearRect(0, 0, 1920, 1080);
+
+    ctx.fillStyle = "white";
+    ctx.fillRect(100, 100, 75, 75);
+
+    requestAnimationFrame(draw);
+  }
+
+  function resize() {
+    // Fix chat messages scrollbar
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+
+    // Resize canvas
+    const scale = Math.min(
+      (window.innerWidth ||
+        (document.documentElement && document.documentElement.clientWidth) ||
+        (document.body && document.body.clientWidth) ||
+        0) / 1920,
+      (window.innerHeight ||
+        (document.documentElement && document.documentElement.clientHeight) ||
+        (document.body && document.body.clientHeight) ||
+        0) / 1080,
+    );
+
+    canvas.width = 1920 * scale;
+    canvas.height = 1080 * scale;
+
+    ctx.scale(scale, scale);
+  }
+}
--- apps/client/src/js/main/index.ts
+++ apps/client/src/js/main/index.ts
@@ -12,9 +12,10 @@ import {
 import { setupDiscordSdk } from "../lib/discord/setup";
 import { handleRoom } from "../lib/server/websocket";
 import { ChatColors, addChatMessage, clearChat } from "../lib/game";
+import { addGameInputs } from "../lib/game/inputs";
 
 import { getGameDocs } from "./docs";
-import { addGameInputs } from "../lib/game/inputs";
+import { setupCanvas } from "./canvas";
 
 main();
 
@@ -122,51 +123,10 @@ export async function setupGame(
     );
   }
 
-  // Gets the canvas
-  const canvas = htmlDocs.canvas;
-  const ctx = htmlDocs.canvas.getContext("2d", {
-    alpha: false,
-  }) as CanvasRenderingContext2D;
+  // Handle canvas
+  setupCanvas(htmlDocs);
 
   // Runs other necessary functions
   clearChat(htmlDocs.chatMessages);
   addGameInputs(htmlDocs, opts);
-
-  // Handles resizing
-  resize();
-  window.onresize = resize;
-
-  // Handles drawing
-  draw();
-
-  function draw() {
-    ctx.clearRect(0, 0, 1920, 1080);
-
-    ctx.fillStyle = "white";
-    ctx.fillRect(100, 100, 75, 75);
-
-    requestAnimationFrame(draw);
-  }
-
-  function resize() {
-    // Fix chat messages scrollbar
-    htmlDocs.chatMessages.scrollTop = htmlDocs.chatMessages.scrollHeight;
-
-    // Resize canvas
-    const scale = Math.min(
-      (window.innerWidth ||
-        (document.documentElement && document.documentElement.clientWidth) ||
-        (document.body && document.body.clientWidth) ||
-        0) / 1920,
-      (window.innerHeight ||
-        (document.documentElement && document.documentElement.clientHeight) ||
-        (document.body && document.body.clientHeight) ||
-        0) / 1080,
-    );
-
-    canvas.width = 1920 * scale;
-    canvas.height = 1080 * scale;
-
-    ctx.scale(scale, scale);
-  }
 }
