0c8d8aeb48eafcbc2783a72caa7e1fcf5feacd48
real2two <26689496+real2two@users.noreply.github.com>
2024-05-15 21:10:25 -0400

    Update vite.config.ts

--- apps/client/vite.config.ts
+++ apps/client/vite.config.ts
@@ -8,12 +8,12 @@ export default ({ mode }) => {
     server: {
       port: parseInt(process.env["VITE_PORT"]!) ?? 5173,
       proxy: {
-        "/api/server/local": {
+        "/api/server/server_id": {
           target: "http://localhost:3002",
           changeOrigin: true,
           secure: false,
           ws: true,
-          rewrite: (path) => path.slice("/api/server/local/".length),
+          rewrite: (path) => path.slice("/api/server/server_id/".length),
         },
         "/api": {
           target: process.env["VITE_PROXY_TARGET"] || "http://localhost:3001",
